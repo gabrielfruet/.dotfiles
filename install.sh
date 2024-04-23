2232d9185e0a2aae9cae2b7109bb988d117d8f1d
Gabriel Fruet <gabrielfruet538@gmail.com>
2024-04-23 20:58:04 -0300

    todo: some thoughts about what should i add

---
+++ install.sh
@@ -0,0 +1,8 @@
+#TODO   
+#       oh-my-zsh
+#       zsh syntax highlighting
+#       zsh auto suggestion 
+#       rofi launcher
+#       nerd font maybe?
+#       remenber of emoji fonts
+#       picom temporarily disabled
