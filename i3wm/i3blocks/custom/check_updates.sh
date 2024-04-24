0b465bd5fc2f4aaebecc42e2becb82e815743c08
Gabriel Fruet <gabrielfruet538@gmail.com>
2024-04-23 21:18:21 -0300

    feat: added support to archlinux in check_updates

--- i3wm/i3blocks/custom/check_updates.sh
+++ i3wm/i3blocks/custom/check_updates.sh
@@ -16,6 +16,26 @@ fedora() {
         symbol=""
     fi
 }
-fedora
+
+archlinux() {
+    UPDATES=$(pacman -Qu)
+    if [ -n "$UPDATES" ]; then
+        color="#FFEB3B" # Yellow
+        txt_color="#131620"
+        symbol=""
+    else
+        color="#9ECE6A" # Green
+        txt_color="#FFFFFF"
+        symbol=""
+    fi
+}
+
+osname="$(hostnamectl | sed -n 's/^ *Operating System: \(.*\)$/\1/p' | sed 's/ //g' | tr '[:upper:]' '[:lower:]')"
+
+case $osname in
+    archlinux) archlinux;;
+    fedora) fedora;;
+esac
+
 
 "$styler_path" "$symbol   " --bg "$color" --txt "$txt_color" --n 0 --sep "\ue0b6"
