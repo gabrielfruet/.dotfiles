ca3a33ea318bb67d76271a9ea747166611b440bc
gabrielfruet <gabrielfruet538@gmail.com>
2024-10-23 21:33:35 -0300

    minor changes

--- zsh/bin/dockerfzf.sh
+++ zsh/bin/dockerfzf.sh
@@ -9,11 +9,11 @@ fi
 # Function to list and stop a running container
 stop_container() {
     local container
-    container=$(sudo docker ps --format '{{.ID}}: {{.Names}}' | fzf --prompt="Select a container to stop: ")
+    container=$(docker ps --format '{{.ID}}: {{.Names}}' | fzf --prompt="Select a container to stop: ")
     
     if [ -n "$container" ]; then
         container_id=$(echo "$container" | cut -d: -f1)
-        sudo docker stop "$container_id"
+        docker stop "$container_id"
         echo "Stopped container: $container"
     else
         echo "No container selected."
@@ -23,11 +23,11 @@ stop_container() {
 # Function to list and start a stopped container
 start_container() {
     local container
-    container=$(sudo docker ps -a --filter "status=exited" --format '{{.ID}}: {{.Names}}' | fzf --prompt="Select a container to start: ")
+    container=$(docker ps -a --filter "status=exited" --format '{{.ID}}: {{.Names}}' | fzf --prompt="Select a container to start: ")
     
     if [ -n "$container" ]; then
         container_id=$(echo "$container" | cut -d: -f1)
-        sudo docker start "$container_id"
+        docker start "$container_id"
         echo "Started container: $container"
     else
         echo "No container selected."
@@ -37,18 +37,18 @@ start_container() {
 # Function to attach to a running container
 attach_container() {
     local container
-    container=$(sudo docker ps --format '{{.ID}}: {{.Names}}' | fzf --prompt="Select a container to attach to: ")
+    container=$(docker ps --format '{{.ID}}: {{.Names}}' | fzf --prompt="Select a container to attach to: ")
     
     if [ -n "$container" ]; then
         container_id=$(echo "$container" | cut -d: -f1)
-        sudo docker exec -it "$container_id" bash
+        docker exec -it "$container_id" bash
     else
         echo "No container selected."
     fi
 }
 
 # Main menu using fzf
-action=$(echo -e "Stop a container\nStart a container\nAttach to a container" | sudo fzf --prompt="Select an action: ")
+action=$(echo -e "Stop a container\nStart a container\nAttach to a container" | fzf --prompt="Select an action: ")
 
 case $action in
     "Stop a container")
