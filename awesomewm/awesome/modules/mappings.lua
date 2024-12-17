282c3a0a49bec790793a7d56bd3df3b4d67f1e17
gabrielfruet <gabrielfruet538@gmail.com>
2024-12-17 19:06:41 -0300

    feat: added keymaps for gmk67 compatibility

--- awesomewm/awesome/modules/mappings.lua
+++ awesomewm/awesome/modules/mappings.lua
@@ -5,9 +5,9 @@ local volhandler = require('modules.handlers.volume')
 local custom_keys = gears.table.join(
 
     -- Volume mapings
-    awful.key({ modkey }, "F1", function() volhandler.toggle_mute() end, {description = "toggle mute", group = "audio"}),
-    awful.key({ modkey }, "F2", function() volhandler.decrease_volume(5) end, {description = "decrease volume by 5%", group = "audio"}),
-    awful.key({ modkey }, "F3", function() volhandler.increase_volume(5) end, {description = "increase volume by 5%", group = "audio"})
+    awful.key({ modkey }, "F10", function() volhandler.toggle_mute() end, {description = "toggle mute", group = "audio"}),
+    awful.key({ modkey }, "F11", function() volhandler.decrease_volume(5) end, {description = "decrease volume by 5%", group = "audio"}),
+    awful.key({ modkey }, "F12", function() volhandler.increase_volume(5) end, {description = "increase volume by 5%", group = "audio"})
 )
 
 return custom_keys
