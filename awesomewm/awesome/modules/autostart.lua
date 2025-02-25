ba66089b597370f11e176f855f36828a3f6f7456
gabrielfruet <gabrielfruet538@gmail.com>
2025-02-25 20:52:41 -0300

    fix: not using dual monitor

--- awesomewm/awesome/modules/autostart.lua
+++ awesomewm/awesome/modules/autostart.lua
@@ -2,7 +2,7 @@ local awful = require('awful')
 
 return {
     run = function()
-        awful.spawn.with_shell(os.getenv('HOME') .. '/.screenlayout/default_dualmonitor.sh')
+        -- awful.spawn.with_shell(os.getenv('HOME') .. '/.screenlayout/default_dualmonitor.sh')
         awful.spawn.with_shell('picom --experimental-backends || picom')
     end
 }
