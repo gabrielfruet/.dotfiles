d5dfb879c97f48d4b3669db9f6b22e0702111d76
gabrielfruet <gabrielfruet538@gmail.com>
2026-03-11 19:30:17 -0300

    feat: picom

--- awesomewm/awesome/modules/autostart.lua
+++ awesomewm/awesome/modules/autostart.lua
@@ -3,7 +3,6 @@ local awful = require('awful')
 return {
     run = function()
         -- awful.spawn.with_shell(os.getenv('HOME') .. '/.screenlayout/default_dualmonitor.sh')
-        awful.spawn.with_shell('picom --experimental-backends || picom')
+        awful.spawn.with_shell('picom -b > ~/.picom.log 2>&1 ')
     end
 }
-
