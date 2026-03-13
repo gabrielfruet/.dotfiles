1a0e352aeb296caf8f91b6ea3b457b8361aaee0d
gabrielfruet <gabrielfruet538@gmail.com>
2026-03-13 08:03:40 -0300

    feat: update

--- zsh/bin/dmenu_screenshot.sh
+++ zsh/bin/dmenu_screenshot.sh
@@ -13,14 +13,15 @@ mkdir -p "$SCROT_FOLDER"
 selected=$(printf "%s\n" "${opts[@]}" | dmenu -l 10)
 
 DELAY=1
+SCROT_COPY_TO_CLIPBOARD=='xclip -selection clipboard -t image/png -i $f'
 
 case $selected in
     "all")
-        scrot -q 100 -d "$DELAY" "$SCROT_FOLDER/screenshot_%Y-%m-%d_%H:%M:%S.png";;
+        scrot -q 100 -d "$DELAY" "$SCROT_FOLDER/screenshot_%Y-%m-%d_%H:%M:%S.png" -e  "$SCROT_COPY_TO_CLIPBOARD";; 
     "select")
-        scrot -q 100 -s --line color=#ff0000 "$SCROT_FOLDER/screenshot_%Y-%m-%d_%H:%M:%S.png";;
+        scrot -q 100 -s --line color="#ff0000" "$SCROT_FOLDER/screenshot_%Y-%m-%d_%H:%M:%S.png" -e "$SCROT_COPY_TO_CLIPBOARD";;
     "focus")
-        scrot -q 100 -u -d "$DELAY" "$SCROT_FOLDER/screenshot_%Y-%m-%d_%H:%M:%S.png";;
+        scrot -q 100 -u -d "$DELAY" "$SCROT_FOLDER/screenshot_%Y-%m-%d_%H:%M:%S.png" -e "$SCROT_COPY_TO_CLIPBOARD";;
     "gui")
         flameshot gui --delay 500
 esac
