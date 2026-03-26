34384feec35e0077378fe30e7fbe3bd00deb114e
gabrielfruet <gabrielfruet538@gmail.com>
2026-03-26 09:02:00 -0300

    fix

--- zsh/zsh-profile/venv.zsh
+++ zsh/zsh-profile/venv.zsh
@@ -6,11 +6,13 @@ autoload -U add-zsh-hook
 # Define the function
 auto_activate_venv() {
     if [[ -f .venv/bin/activate ]]; then
-        export VIRTUAL_ENV_DISABLE_PROMPT=1
         source .venv/bin/activate
     # Deactivate if leaving the environment directory
     elif [[ -n "$VIRTUAL_ENV" && ! -f .venv/bin/activate ]]; then
-        deactivate
+        if typeset -f deactivate > /dev/null; then
+            VIRTUAL_ENV=""
+            deactivate
+        fi
     fi
 }
 
