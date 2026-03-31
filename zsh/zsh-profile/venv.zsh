600c3c0f0d64abac6a9d8d5ec276c2d016e60b7a
gabrielfruet <gabrielfruet538@gmail.com>
2026-03-31 09:08:40 -0300

    fix

--- zsh/zsh-profile/venv.zsh
+++ zsh/zsh-profile/venv.zsh
@@ -1,22 +1,15 @@
 #!/bin/env zsh
 
-# Ensure the hook system is loaded
 autoload -U add-zsh-hook
 
-# Define the function
 auto_activate_venv() {
     if [[ -f .venv/bin/activate ]]; then
+        export VIRTUAL_ENV_DISABLE_PROMPT=1
         source .venv/bin/activate
-    # Deactivate if leaving the environment directory
-    elif [[ -n "$VIRTUAL_ENV" && ! -f .venv/bin/activate ]]; then
-        if typeset -f deactivate > /dev/null; then
-            VIRTUAL_ENV=""
-            deactivate
-        fi
+    elif [[ -n "$VIRTUAL_ENV" ]]; then
+        export VIRTUAL_ENV=""
     fi
 }
 
-# Attach the function to the directory change event
 add-zsh-hook chpwd auto_activate_venv
-
-auto_activate_venv  # Run it once for the current directory
+auto_activate_venv
